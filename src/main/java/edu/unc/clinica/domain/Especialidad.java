f1e0d7340946b16c77c0422002ba6c01e763c559
BFernand0 <bchilonj20_1@unc.edu.pe>
2024-03-05 00:26:35 -0500

    Documentación de la clase Especialidad

--- src/main/java/edu/unc/clinica/domain/Especialidad.java
+++ src/main/java/edu/unc/clinica/domain/Especialidad.java
@@ -1,5 +1,7 @@
+// Define el paquete al que pertenece la clase
 package edu.unc.clinica.domain;
 
+//Importa las clases necesarias
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,27 +9,32 @@ import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
-
+//Anotación que indica que esta clase es una entidad JPA
 @Entity
+//Anotación de Lombok que agrega automáticamente los métodos getter, setter, equals, hashCode y toString
 @Data
 public class Especialidad {
-	
+	// Identificador único generado automáticamente para la especialidad
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idEspecialidad;
 	
+	// Anotación que especifica que el nombre de la especialidad no puede estar en blanco
 	@NotBlank(message = "El nombre de la especialidad no puede estar vacío.")
 	private String nombreEsp;
 	
+	// Anotación que ignora la serialización de la lista de médicos al formato JSON
 	@JsonIgnore
+	
+	// Relación uno a muchos con la clase Medico, mapeada por el campo "especialidad" en la clase Medico
 	@OneToMany(mappedBy="especialidad")
 	private List<Medico> medicos=new ArrayList<>();
-
+	// Constructor por defecto
+	// (Lombok genera automáticamente el constructor con todos los parámetros)
 }
