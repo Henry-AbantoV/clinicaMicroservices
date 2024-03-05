db03c7455e4b4201843bad4af684c361fd6d4a9a
BFernand0 <bchilonj20_1@unc.edu.pe>
2024-03-05 01:24:18 -0500

    Documentacion de CitaServiceImp

--- src/main/java/edu/unc/clinica/services/CitaServiceImp.java
+++ src/main/java/edu/unc/clinica/services/CitaServiceImp.java
@@ -14,7 +14,10 @@ import edu.unc.clinica.exceptions.ErrorMessage;
 import edu.unc.clinica.exceptions.IllegalOperationException;
 import edu.unc.clinica.repositories.CitaRepository;
 import edu.unc.clinica.repositories.FacturaRepository;
-
+/**
+ * La clase CitaServiceImp implementa la interfaz CitaService y proporciona la lógica de negocio para operaciones relacionadas con citas y facturas en el sistema.
+ * Está marcada con la anotación @Service para indicar que es un componente de servicio gestionado por Spring.
+ */
 @Service
 public class CitaServiceImp implements CitaService {
 
@@ -24,11 +27,23 @@ public class CitaServiceImp implements CitaService {
 	@Autowired
 	private FacturaRepository facturaR;
 	
+	/**
+     * Recupera una lista de todas las citas almacenadas en el sistema.
+     *
+     * @return Lista de citas.
+     */
 	@Override
 	public List<Cita> listarCitas() {
 		return (List<Cita>)citaR.findAll();
 	}
 
+	/**
+     * Busca una cita por su identificador único.
+     *
+     * @param idCita Identificador único de la cita.
+     * @return Cita correspondiente al ID proporcionado.
+     * @throws EntityNotFoundException Si no se encuentra la cita con el ID especificado.
+     */
 	@Override
 	@Transactional(readOnly=true)
 	public Cita buscarCitabyId(Long IdCita) throws EntityNotFoundException  {
@@ -36,18 +51,41 @@ public class CitaServiceImp implements CitaService {
 		return cita.get();
 		}
 
+	/**
+     * Guarda una nueva cita en el sistema.
+     *
+     * @param cita La cita a ser guardada.
+     * @return Cita guardada.
+     * @throws IllegalOperationException Si la operación no es válida.
+     */
 	@Override
 	@Transactional
 	public Cita grabarCita(Cita cita) throws IllegalOperationException  {
 		return citaR.save(cita);
 	}
 
+	/**
+     * Elimina una cita del sistema por su identificador único.
+     *
+     * @param idCita Identificador único de la cita a ser eliminada.
+     * @throws EntityNotFoundException  Si no se encuentra la cita con el ID especificado.
+     * @throws IllegalOperationException Si la operación no es válida.
+     */
 	@Override
 	public void eliminarCita(Long IdCita) throws EntityNotFoundException, IllegalOperationException {
 		citaR.deleteById(IdCita);
 		
 	}
 	
+	/**
+     * Actualiza la información de una cita existente en el sistema.
+     *
+     * @param id   Identificador único de la cita a ser actualizada.
+     * @param cita Nueva información de la cita.
+     * @return Cita actualizada.
+     * @throws EntityNotFoundException  Si no se encuentra la cita con el ID especificado.
+     * @throws IllegalOperationException Si la operación no es válida.
+     */
 	@Override
 	@Transactional
 	public Cita actualizarCita(Long id, Cita cita) throws EntityNotFoundException, IllegalOperationException {
@@ -59,7 +97,15 @@ public class CitaServiceImp implements CitaService {
 		return citaR.save(cita);
 	}
 
-	
+	/**
+     * Asigna una factura a una cita existente en el sistema.
+     *
+     * @param idCita    Identificador único de la cita.
+     * @param idFactura Identificador único de la factura a ser asignada.
+     * @return Cita actualizada con la asignación de factura.
+     * @throws EntityNotFoundException  Si no se encuentra la cita o la factura con los ID especificados.
+     * @throws IllegalOperationException Si la operación no es válida.
+     */
 	@Override
 	@Transactional
 	public Cita asignarFactura(Long IdCita, Long IdFactura) throws EntityNotFoundException, IllegalOperationException {
