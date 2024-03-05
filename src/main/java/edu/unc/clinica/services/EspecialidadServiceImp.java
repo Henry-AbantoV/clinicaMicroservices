020386a7a01ac1b5f0d66e4695011dc560100306
Ever Henry Abanto Vilchez <eabantov20_1@unc.edu.pe>
2024-03-05 00:11:43 -0500

    Documentacion de EspecialidadServiceImp

--- src/main/java/edu/unc/clinica/services/EspecialidadServiceImp.java
+++ src/main/java/edu/unc/clinica/services/EspecialidadServiceImp.java
@@ -17,6 +17,9 @@ import edu.unc.clinica.exceptions.IllegalOperationException;
 import edu.unc.clinica.repositories.EspecialidadRepository;
 import edu.unc.clinica.repositories.MedicoRepository;
 
+/**
+ * Implementación de la interfaz EspecialidadService que define los servicios relacionados con las especialidades médicas.
+ */
 @Service
 public class EspecialidadServiceImp  implements EspecialidadService{
 
@@ -26,11 +29,23 @@ public class EspecialidadServiceImp  implements EspecialidadService{
 	@Autowired
 	MedicoRepository medicoR;
 	
+	 /**
+     * Método para listar todas las especialidades médicas.
+     *
+     * @return Lista de objetos de tipo Especialidad.
+     */
 	@Override
 	public List<Especialidad> listarEspecialidades() {
 		return EspR.findAll();
 	}
 
+	/**
+     * Método para buscar una especialidad médica por su ID.
+     *
+     * @param IdEsp ID de la especialidad médica a buscar.
+     * @return Objeto Especialidad correspondiente al ID especificado.
+     * @throws EntityNotFoundException Si no se encuentra la especialidad médica con el ID especificado.
+     */
 	@Override
 	@Transactional(readOnly=true)
 	public Especialidad buscarEspecialidadbyId(Long IdEsp) throws EntityNotFoundException {
@@ -38,11 +53,27 @@ public class EspecialidadServiceImp  implements EspecialidadService{
 		return espec.get();
 	}
 
+	/**
+     * Método para grabar una nueva especialidad médica.
+     *
+     * @param espec Objeto de tipo Especialidad a ser grabado.
+     * @return La especialidad médica grabada.
+     * @throws IllegalOperationException Si se produce una operación ilegal al intentar grabar la especialidad médica.
+     */
 	@Override
 	public Especialidad grabarEspecilidad(Especialidad espec) throws IllegalOperationException {
 		return EspR.save(espec);
 	}
 
+	 /**
+     * Método para actualizar una especialidad médica existente.
+     *
+     * @param id    ID de la especialidad médica a actualizar.
+     * @param espec Objeto de tipo Especialidad con los datos actualizados.
+     * @return La especialidad médica actualizada.
+     * @throws EntityNotFoundException    Si no se encuentra la especialidad médica con el ID especificado.
+     * @throws IllegalOperationException Si se produce una operación ilegal al intentar actualizar la especialidad médica.
+     */
 	@Override
 	public Especialidad actualizarEspecilidad(Long id, Especialidad espec) throws EntityNotFoundException, IllegalOperationException{
 	Optional<Especialidad> espEntity = EspR.findById(id);
@@ -53,12 +84,29 @@ public class EspecialidadServiceImp  implements EspecialidadService{
 	return EspR.save(espec);
 	}
 
+	/**
+     * Método para eliminar una especialidad médica por su ID.
+     *
+     * @param IdEsp ID de la especialidad médica a eliminar.
+     * @throws EntityNotFoundException    Si no se encuentra la especialidad médica con el ID especificado.
+     * @throws IllegalOperationException Si se produce una operación ilegal al intentar eliminar la especialidad médica.
+     */
 	@Override
 	public void eliminarEspecialidad(Long IdEsp) throws EntityNotFoundException, IllegalOperationException {
 		EspR.deleteById(IdEsp);
 		
 	}
 	
+	
+	 /**
+     * Método para asignar médicos a una especialidad médica.
+     *
+     * @param IdEsp   ID de la especialidad médica a la que se asignarán los médicos.
+     * @param IdMedico ID del médico a asignar a la especialidad médica.
+     * @return La especialidad médica actualizada con el médico asignado.
+     * @throws EntityNotFoundException    Si no se encuentra la especialidad médica o el médico con los IDs especificados.
+     * @throws IllegalOperationException Si se produce una operación ilegal al intentar asignar el médico a la especialidad médica.
+     */
 	@Override
 	@Transactional
 	public Especialidad asignarMedicos(Long IdEsp, Long IdMedico) throws EntityNotFoundException, IllegalOperationException {
